3ca07a6553c15129902736537e39247006bba784
James Bliss <james@ridersdiscount.com>
2013-06-27 17:06:22 -0400

    Only format validation exceptions if there's a % in them

--- mongokit/schema_document.py
+++ mongokit/schema_document.py
@@ -625,9 +625,12 @@ class SchemaDocument(dict):
                             raise ValidationError(
                               "%s does not pass the validator " + validator.__name__)
                     except Exception, e:
-                        self._raise_exception(ValidationError, key,
-                          unicode(e) % (key)
-                        )
+                        if '%' in unicode(e):
+                            self._raise_exception(ValidationError, key,
+                              unicode(e) % (key)
+                            )
+                        else:
+                            raise
 
     def _process_custom_type(self, target, doc, struct, path = "", root_path=""):
         for key in struct:
